1c5cac74e015a2e8cd8b300a8aeebd92f129e1f0
Anatol Lupacescu <anatollupacescu@gmail.com>
2021-04-02 10:13:54 +0300

    move kad to topology package, add light nodes field to debug api

--- pkg/topology/topology.go
+++ pkg/topology/topology.go
@@ -98,4 +98,5 @@ type KadParams struct {
 	NNLowWatermark int       `json:"nnLowWatermark"` // low watermark for depth calculation
 	Depth          uint8     `json:"depth"`          // current depth
 	Bins           KadBins   `json:"bins"`           // individual bin info
+	LightNodes     BinInfo   `json:"lightNodes"`     // light nodes bin info
 }
