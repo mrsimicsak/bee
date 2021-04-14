511026594463bb90350d296b6386e9498f873aeb
Anatol Lupacescu <anatollupacescu@gmail.com>
2021-04-14 10:27:06 +0300

    add copyright, fix comments

--- pkg/p2p/specwrapper.go
+++ pkg/p2p/specwrapper.go
@@ -1,3 +1,7 @@
+// Copyright 2021 The Swarm Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
 package p2p
 
 import (
@@ -8,7 +12,7 @@ import (
 
 var ErrUnexpected = errors.New("unexpected request while in light mode")
 
-// WithDisconnectStreams will mutate the given spec and replace the handler with a always erroring one
+// WithDisconnectStreams will mutate the given spec and replace the handler with a always erroring one.
 func WithDisconnectStreams(spec ProtocolSpec) {
 	for i := range spec.StreamSpecs {
 		spec.StreamSpecs[i].Handler = func(c context.Context, p Peer, s Stream) error {
@@ -17,7 +21,7 @@ func WithDisconnectStreams(spec ProtocolSpec) {
 	}
 }
 
-// WithBlocklistStreams will mutate the given spec and replace the handler with a always erroring one
+// WithBlocklistStreams will mutate the given spec and replace the handler with a always erroring one.
 func WithBlocklistStreams(dur time.Duration, spec ProtocolSpec) {
 	for i := range spec.StreamSpecs {
 		spec.StreamSpecs[i].Handler = func(c context.Context, p Peer, s Stream) error {
